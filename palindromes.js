6e1baf8d5b22281950f887b4bee84455e6f1fd85
HasinJ <hasinchoudhuryj@gmail.com>
2020-08-12 18:38:55 -0400

    third test

--- palindromes.js
+++ palindromes.js
@@ -1,5 +1,7 @@
 const palindromes = function(string) {
-  end = Array.from(string).reverse().join("") == string ? true:false
+  string = string.replace(/[^a-zA-Z ]/g, "").toLowerCase()
+  rString = Array.from(string).reverse().join("").toLowerCase()
+  end = rString == string ? true:false
   return end
 };
 
